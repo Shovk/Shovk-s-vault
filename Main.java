9bc4e9684339ffa4c6e7ed26d17b2e9e870a92f1
Shovk <31659156+Shovk@users.noreply.github.com>
2017-09-05 17:44:31 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.gmail.shovkv;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cat catOne = new Cat("Avgur Tutsi Shaterier", "Scottish fold", "Spotted tabby", 5, 15, "Male");
+		catOne.print();
+	}
+}
